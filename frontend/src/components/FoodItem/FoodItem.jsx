c834eb0965642e9675714b6588013123df6ef0c4
RyanRanjith <kingranjith4@gmail.com>
2024-06-12 11:26:46 -0700

    a

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -6,6 +6,8 @@ import { StoreContext } from '../../context/StoreContext'
 
 const FoodItem = ({id,name,price,description,image}) => {
 
+    
+
     const {cartItems,addToCart,removeFromCart} = useContext(StoreContext);
 
 
@@ -18,9 +20,9 @@ const FoodItem = ({id,name,price,description,image}) => {
             {!cartItems[id]
                ?<img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt="" />
                :<div className='food-item-counter'>
-                <img onClick={()=>removeFromCart(id)}src={assets.remove_icon_red} alt="" />
+                <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
                 <p>{cartItems[id]}</p>
-                <img onClick={()=>addToCart(id)}src={assets.add_icon_green} alt="" />
+                <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt="" />
                 </div>
             }
         </div>
