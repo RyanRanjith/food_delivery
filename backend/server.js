a861149d5b6765aa6048f47937e3f6dd25995c88
RyanRanjith <kingranjith4@gmail.com>
2024-07-11 08:29:56 -0700

    a

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
+import 'dotenv/config'
 
 // app config
 const app = express()
@@ -19,7 +20,7 @@ connectDB();
 // api endpoints
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
-app.use("api/user",userRouter)
+app.use("/api/user",userRouter)
 
 
 app.get("/",(req,res)=>{
