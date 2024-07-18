93962e2ffff87b6db3926eef5364e903c94c6712
RyanRanjith <kingranjith4@gmail.com>
2024-07-18 08:25:46 -0700

    a

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -27,7 +27,7 @@ const placeOrder = async (req,res) => {
                 product_data:{
                     name:item.name
                 },
-                unit_amount:item.price*100
+                unit_amount:item.price*100*80
              },
              quantity:item.quantity
 
@@ -39,7 +39,7 @@ const placeOrder = async (req,res) => {
                 product_data:{
                     name:"Delivery Charges"
                 },
-                unit_amount:2*100
+                unit_amount:2*100*80
             },
             quantity:1
         })
@@ -76,4 +76,15 @@ const verifyOrder = async (req,res) => {
   }
 }
 
-export {placeOrder,verifyOrder}
+// user orders for frontend
+const userOrders = async (req,res) => {
+   try {
+      const orders = await orderModel.find({userId:req.body.userId});
+      res.json({success:true,data:orders})
+   } catch (error) {
+    console.log(error);
+    res.json({success:false,message:"Error"})
+   }
+}
+
+export {placeOrder,verifyOrder,userOrders}
